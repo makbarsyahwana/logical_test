5f3140f7a5eb2c4533d40cfb17a9822471b88fdb
makbarsyahwana <makbarsyahwana@gmail.com>
2021-07-04 18:08:45 +0700

    change variable name

--- primesNumber.js
+++ primesNumber.js
@@ -1,23 +1,23 @@
-function getPrimes(num) {
-    let primes
+function getFactor(num) {
+    let factor
     let result = 0
     for (let i=12; i<=num; i++) {
-        primes = []
+        factor = []
         for(let j=2; j<=i; j++) {
             if (i % j === 0) {
-                primes.push(j)
-                if (primes.length === 6) {
+                factor.push(j)
+                if (factor.length === 6) {
                     break
                 }
             }
         }
         // console.log(i, primes)
-        if (primes.length === 5) {
-            primes = []
+        if (factor.length === 5) {
+            factor = []
             result++
         }
     }
     console.log(result)
 }
 
-getPrimes(262144)
+getFactor(134217728)
